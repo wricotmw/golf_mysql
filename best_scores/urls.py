81a92d1d28cb33ed7b78c7e9d01902bb5d8526e9
wricotmw <terrymw@wrico.co.uk>
2023-11-01 16:25:51 +0000

    addgolfer and scores

--- best_scores/urls.py
+++ best_scores/urls.py
@@ -1,8 +1,10 @@
 
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name= "home"),
+    path('add_golfer/', views.add_golfer, name= "add_golfer"),
+    path('add_scores/', views.add_scores, name= "add_scores"),
 ]
--- best_scores/views.py
+++ best_scores/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from .models import Scores, Golfer, FiveScores
+from .forms import ScoreForm, GolferForm
+from django.http import HttpResponseRedirect
+#from .utils import get_scores
 
 # Create your views here.
 def home(request):
 	return render(request, 'best_scores/home.html', {})
+
+
+def add_golfer(request):
+	submitted = False
+	if request.method == 'POST':
+		form = GolferForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_golfer/?submitted=True')
+	else:
+		form = GolferForm()
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 
+		'best_scores/add_golfer.html',
+		{'form': form, 'submitted': submitted}
+		)
+
+def add_scores(request):
+	submitted = False
+	if request.method == 'POST':
+		form = ScoreForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_scores/?submitted=True')
+	else:
+		form = ScoreForm()
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 
+		'best_scores/add_scores.html',
+		{'form': form, 'submitted': submitted}
+		)
