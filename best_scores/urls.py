11ff6117efe242cb18dda0ad746afce20ce7267f
wricotmw <terrymw@wrico.co.uk>
2023-11-05 15:20:12 +0000

    add fuctionality

--- best_scores/urls.py
+++ best_scores/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home, name= "home"),
     path('add_golfer/', views.add_golfer, name= "add_golfer"),
     path('add_scores/', views.add_scores, name= "add_scores"),
+    path('scores/', views.all_scores, name= "all_scores"),
+    path('league/', views.league, name= "league_table"),
 ]
--- best_scores/views.py
+++ best_scores/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from .models import Scores, Golfer, FiveScores
 from .forms import ScoreForm, GolferForm
 from django.http import HttpResponseRedirect
-#from .utils import get_scores
+from .utils import get_scores
 
 # Create your views here.
 def home(request):
@@ -43,3 +43,35 @@ def add_scores(request):
 		'best_scores/add_scores.html',
 		{'form': form, 'submitted': submitted}
 		)
+
+def all_scores(request):
+	score_list = Scores.objects.all().order_by('score_date')
+
+	return render(request, 'best_scores/score_list.html', {
+		'score_list':score_list
+		})
+
+
+def league(request):
+	golfer_list = Golfer.objects.all()
+	
+	FiveScores.objects.all().delete()
+	golfer_scores = []
+	for name in golfer_list:
+		golfer_scores.append(name)
+		score_list = get_scores(name)
+		#for score in score_list:
+		#	golfer_scores.append(score)
+	# now sort into descending order and slice top 5
+	#see utils.py
+	# now craete record for the database and write to FiveScores
+
+	#now read from Fivescores.all and return to web page
+
+		
+	return render(request, 'best_scores/league_table.html', {
+			'score_list':score_list,
+			#'golfer_scores': golfer_scores,
+			
+			
+			})
